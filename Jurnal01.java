4f3ab4b9be9a208d08106cdd13c8c8b39914dea5
Yehezkielkiel04 <yehezkielfredy064+1@gmail.com>
2023-09-27 09:46:07 +0700

    Kumpul Jurnal01

---
+++ Jurnal01.java
@@ -0,0 +1,11 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        String nama = "YEHEZKIEL THEYSA FREDY";
+        String nim = "607062300091";
+        String kelas = "04";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Nim: " + nim);
+        System.out.println("Kelas: " + kelas);
+    }
+}
